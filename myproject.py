a9a8439a5f51865c70dffc083197e0d98f416745
Zegoris <zegor39@gmail.com>
2022-12-07 23:41:40 +0300

    Add painting objects

--- myproject.py
+++ myproject.py
@@ -1,18 +1,19 @@
 import pygame
 import json
 from random import choice
+
 pygame.init()
 pygame.mixer.init()
 
 
-class Board: # General class for game modes
+class Board:  # General class for game modes
     def __init__(self, screen, width, height):
         self.width = width
         self.height = height
-        self.board = [[0] * width for _ in range(height)] # Matrix of values of painted cells
+        self.board = [[0] * width for _ in range(height)]  # Matrix of values of painted cells
         self.left = 0
         self.top = 0
-        self.cell_size = 40
+        self.cell_size = 35
         self.screen = screen
         self.lightTheme = (245, 240, 225)
         self.darkTheme = (30, 61, 89)
@@ -28,23 +29,22 @@ class Board: # General class for game modes
         else:
             screen.fill(self.lightTheme)
 
-    def cell(self, x, y): # Getting the coordinate of the cell in the matrix
+    def cell(self, x, y):
         if self.width * self.cell_size + self.left >= x >= self.left \
                 and self.height * self.cell_size + self.top >= y >= self.top:
             row, col = abs((x - self.left) // self.cell_size), \
-                       abs((y - self.top) // self.cell_size)
+                abs((y - self.top) // self.cell_size)
             self.negative(row, col)
 
-    def negative(self, row, col): # Painting the cell
-        color = choice([1, 2, 3, 4, 5]) # Selecting the color of the object
-        object = choice([[(-2, 0), (-1, 0), (0, 0), (1, 0)], # Selecting the form of the object
-                         [(-1, 0), (0, 0), (1, 0), (0, 1)],
-                         [(-1, 0), (0, 0), (0, 1), (-1, 1)],
-                         [(-1, 0), (0, 0), (1, 0), (1, 1)],
-                         [(0, 0), (1, 0), (0, 1), (-1, 1)]])
+    def negative(self, row, col):  # Painting the cell
+        color = choice([1, 2, 3, 4, 5])  # Selecting the color of the object
+        object = choice([[(0, 0), (0, -1), (0, 1), (1, 0)],  # Selecting the form of the object
+                         [(0, 0), (0, 1), (1, 1), (1, 0)],
+                         [(0, 0), (0, -1), (0, 1), (1, 1)],
+                         [(0, 0), (0, -1), (1, 1), (1, 0)],
+                         [(0, 0), (0, -1), (0, 1), (0, 2)]])
         for i in object:
             self.board[col + i[0]][row + i[1]] = color
-        print(self.board)
 
     def render(self):
         if self.theme:
@@ -89,19 +89,24 @@ class Board: # General class for game modes
                                       col * self.cell_size + self.top,
                                       self.cell_size,
                                       self.cell_size), 0)
+        pygame.display.flip()
 
 
-class First_GM(Board): # Class of the first game mode
+class First_GM(Board):  # Class of the first game mode
     def __init__(self):
-        self.size = self.width, self.height = 500, 700  # Window Size
+        self.size = self.width, self.height = 600, 700  # Window Size
         self.screen = pygame.display.set_mode(self.size)  # Screen Setting
         running = True
-        Board.__init__(self, self.screen, 12, 18)
+        Board.__init__(self, self.screen, 12, 20)
         self.render()  # draw board
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:  # Stop Programm
                     running = False
+                if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                    self.cell(*event.pos)
+            self.render()
+            pygame.display.flip()
 
 
-First_GM()
+game_screen = First_GM()
